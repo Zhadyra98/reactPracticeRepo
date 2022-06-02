a923524d6303ada621c05a3db7b8be1e218ebf2c
Zhadyra Oralkhanova <oralkhanova.zhadyra@gmail.com>
2022-06-02 11:50:14 +0300

    sostoyanie componentov

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import { Component } from 'react';
 import './App.css';
 
-
-
 class WhoAmI extends Component{
   constructor(props) {
     super(props);
     this.state = {//динамическое Состояние- объект , а пропс мы не можем менять они только для чтения
-      years: 27
+      years: 27,
+      text:'Add year'
     }
   }
+
+  nextYear = () => {
+    console.log("+++");
+    this.setState(state => ({//Вызывается рендер компонента, если наш новый стейт зависит от старого, мы используем коллбэк функцию, он будет ожидать пока наш старый стейт будет готов к применению
+        years: state.years+1 // а если не зависит от пред. значения, то просто передаем новое значение
+    }))
+  }
+
   render() {
     const {name, surname, link} = this.props;
     return (
       <div className="class1">
-        <h1>My name is {name},  surname - {surname}</h1>
+        <button onClick={this.nextYear}>{this.state.text}</button> {/*Eto analog addEventListener */}
+        <h1>My name is {name},  surname - {surname}, age - {this.state.years}</h1>
         <a href={link}>My profile</a>
       </div>
     )
@@ -25,8 +33,7 @@ function App() {
   return (
     <div className="App">
         <WhoAmI name='John' surname='Smith' link='facebook.com'/>
-        <WhoAmI name='Alex' surname='Almad' link='vk.com'/>
-        
+        <WhoAmI name='Alex' surname='Almad' link='vk.com'/>     
     </div>
   );
 }
@@ -34,3 +41,6 @@ function App() {
 export default App;
 
 /* В Пропс как вы видите  можно передавать все что угодно, но будь осторожна с Объектами ,т.к по нельзя напрямую их всавлять  */
+//1)у компонетов имеются внутренние стейты
+//2)стейты напрямую менять нельзя используем setState()
+//3)setState()- asynchronic operation
