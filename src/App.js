184f5a0538969833f6253c7c6e45714e05c96e90
Zhadyra Oralkhanova <oralkhanova.zhadyra@gmail.com>
2022-06-02 11:12:11 +0300

    Функциональный компонент был изменен в Классовый, он наследуется от РЕАКТ.КОМПОНЕНТ

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
+import { Component } from 'react';
 import './App.css';
 
 
-// function WhoAmI (props) {  Объект пропс форм. по умолчания, можно и так написать, но второй вариант чаще исполь
-//   return (
-//     <div className="class1">
-//       <h1>My name is {props.name},  surname - {props.surname}</h1>  Пропс не динамический здесь мы не можем менять его занчения 
-//       <a href={props.link}>My profile</a>
-//     </div>//Объекты не можем вставлять напрямую JSX
-//   )
-// }
 
-function WhoAmI ({name, surname, link}) {
-  return (
-    <div className="class1">
-      <h1>My name is {name},  surname - {surname}</h1>
-      <a href={link}>My profile</a>
-    </div>
-  )
+class WhoAmI extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {//динамическое Состояние- объект , а пропс мы не можем менять они только для чтения
+      years: 27
+    }
+  }
+  render() {
+    const {name, surname, link} = this.props;
+    return (
+      <div className="class1">
+        <h1>My name is {name},  surname - {surname}</h1>
+        <a href={link}>My profile</a>
+      </div>
+    )
+  }
 }
 
 function App() {
   return (
     <div className="App">
-        <WhoAmI name={() => {return 'John'}} surname='Smith' link='facebook.com'/>
-        <WhoAmI name={() => {return 'John'}} surname='Almad' link='vk.com'/>
-        {/* <WhoAmI name={{firstName : 'Alex'}} surname='Smith' link='facebook.com'/>
-        <WhoAmI name={{firstName : 'Alex'}} surname='Almad' link='vk.com'/>
-        <WhoAmI name={'Sam'} surname='Smith' link='facebook.com'/>
-        <WhoAmI name={'Sam'} surname='Almad' link='vk.com'/> */}
-{/* В Пропс как вы видите  можно передавать все что угодно, но будь осторожна с Объектами ,т.к по нельзя напрямую их всавлять  */}
+        <WhoAmI name='John' surname='Smith' link='facebook.com'/>
+        <WhoAmI name='Alex' surname='Almad' link='vk.com'/>
+        
     </div>
   );
 }
 
 export default App;
+
+/* В Пропс как вы видите  можно передавать все что угодно, но будь осторожна с Объектами ,т.к по нельзя напрямую их всавлять  */
